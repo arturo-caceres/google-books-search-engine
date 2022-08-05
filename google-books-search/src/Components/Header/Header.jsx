84fccdb1df39c53a70dc971735697d01d5d963f3
arturo-caceres <arturo.caceres02383@gmail.com>
2022-08-05 17:24:34 +1000

    created header component

---
+++ google-books-search/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <div className={styles.mainTitle}>
+            <h1>Collections of Books</h1>
+        </div>
+    );
+};
+
+export default Header;
